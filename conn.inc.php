d8ecc09828d23a05da4909d9dbf2bb6c53b44e75
Manikanth <manikanth.iitkgp@gmail.com>
2018-04-10 11:29:56 +0530

    .

--- conn.inc.php
+++ conn.inc.php
@@ -1,12 +1,12 @@
 <?php
-$host_name = "localhost";
-$user_name = "root";
-$pass = "";
+$host_name = "db733487241.db.1and1.com";
+$user_name = "dbo733487241";
+$pass = "viswajeet123";
 
 $mysql_connect = mysqli_connect($host_name , $user_name , $pass);
 $mysql_db = mysqli_select_db($mysql_connect ,"my_database");
 if((!$mysql_connect) || (!$mysql_db))
 {
 	die(mysqli_error($mysql_connect));
-}	
+}
 ?>
